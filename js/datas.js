549685dfd1c0df51e5a825461f431ffafcf2da96
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-10-01 15:45:27 +0700

    feat: project page

---
+++ js/datas.js
@@ -0,0 +1,108 @@
+const tech = [
+  {
+    name: 'JavaScript',
+    src: './svg/icons8-javascript.svg',
+    level: 'Intermediate',
+  },
+  {
+    name: 'HTML',
+    src: './svg/icons8-html-5.svg',
+    level: 'Advance',
+  },
+  {
+    name: 'CSS',
+    src: './svg/icons8-css.svg',
+    level: 'Advance',
+  },
+  {
+    name: 'Bootstrap',
+    src: './svg/icons8-bootstrap.svg',
+    level: 'Advance',
+  },
+  {
+    name: 'Express JS',
+    src: './svg/icons8-express-js.svg',
+    level: 'Beginner',
+  },
+  {
+    name: 'Node JS',
+    src: './svg/icons8-node-js.svg',
+    level: 'Beginner',
+  },
+  {
+    name: 'React JS',
+    src: './svg/icons8-react.svg',
+    level: 'Beginner',
+  },
+  {
+    name: 'Tailwind CSS',
+    src: './svg/icons8-tailwind-css.svg',
+    level: 'Beginner',
+  },
+  {
+    name: 'Next JS',
+    src: './svg/nextjs-icon-svgrepo-com.svg',
+    level: 'Beginner',
+  },
+  {
+    name: 'MongoDB',
+    src: './svg/icons8-mongodb.svg',
+    level: 'Beginner',
+  },
+  {
+    name: 'CodeIgniter 4',
+    src: './svg/codeigniter.svg',
+    level: 'intermediate',
+  },
+  {
+    name: 'MySQL',
+    src: './svg/icons8-mysql.svg',
+    level: 'intermediate',
+  },
+];
+
+const tools = [
+  {
+    name: 'Figma',
+    src: './svg/icons8-figma.svg',
+    level: 'UI Design',
+  },
+  {
+    name: 'Postman',
+    src: './svg/icons8-postman.png',
+    level: 'API',
+  },
+  {
+    name: 'Github',
+    src: './svg/icons8-github.svg',
+    level: 'Repository',
+  },
+  {
+    name: 'Git',
+    src: './svg/icons8-git.svg',
+    level: 'Version Control',
+  },
+];
+
+const projects = [
+  {
+    id: 1,
+    name: 'Landing Page GWA',
+    image: './img/landingpagegwa.png',
+    github: 'https://github.com/HamzahRaihan/landing-page-gwa',
+    link: 'https://landing-page-gwa.vercel.app/',
+    desc: 'GreenWorldAware is a website for those of you who are environmentally aware, providing information, tips and platforms to help you make a clean contribution to a cleaner earth.',
+    stack: ['./svg/icons8-css.svg', './svg/icons8-javascript.svg', './svg/icons8-html-5.svg'],
+  },
+  {
+    id: 2,
+    name: 'Landing Page GWA',
+    image: './img/landingpagegwa.png',
+    github: 'https://github.com/HamzahRaihan/landing-page-gwa',
+    link: 'https://landing-page-gwa.vercel.app/',
+    desc: 'GreenWorldAware is a website for those of you who are environmentally aware, providing information, tips and platforms to help you make a clean contribution to a cleaner earth.',
+    stack: ['./svg/icons8-css.svg', './svg/icons8-javascript.svg', './svg/icons8-html-5.svg'],
+  },
+];
+
+export { tech, tools, projects };
