43f402bba9c943a6f64fecb77e8b7d3b545a4972
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-10-06 13:42:13 +0700

    feat: added module for DOM

---
+++ js/dom/dom.js
@@ -0,0 +1,7 @@
+const navbar = document.querySelector('.navbar');
+const burgerButton = document.querySelector('.dropdown-button');
+const exitButton = document.querySelector('.exit');
+const sidebar = document.querySelector('.sidebar');
+const overlay = document.querySelector('.overlay');
+
+export { navbar, burgerButton, exitButton, sidebar, overlay };
