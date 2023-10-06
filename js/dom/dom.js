0812fb4e168e6743aa11c7b9c2680e754a8d5dcc
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-10-06 13:49:20 +0700

    feat: added dom selector

--- js/dom/dom.js
+++ js/dom/dom.js
@@ -3,5 +3,6 @@ const burgerButton = document.querySelector('.dropdown-button');
 const exitButton = document.querySelector('.exit');
 const sidebar = document.querySelector('.sidebar');
 const overlay = document.querySelector('.overlay');
+const blogContainer = document.querySelector('.blog');
 
-export { navbar, burgerButton, exitButton, sidebar, overlay };
+export { navbar, burgerButton, exitButton, sidebar, overlay, blogContainer };
