e76a208feefa86ed5db5e5142758a56bd3912591
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-10-06 14:08:51 +0700

    feat: added more dom selectors

--- js/dom/dom.js
+++ js/dom/dom.js
@@ -4,5 +4,11 @@ const exitButton = document.querySelector('.exit');
 const sidebar = document.querySelector('.sidebar');
 const overlay = document.querySelector('.overlay');
 const blogContainer = document.querySelector('.blog');
+const selected = document.querySelectorAll('.select div');
+const selectContainer = document.querySelector('.select-detail');
+const toolsCardContainer = document.querySelector('.tools-container');
+const techCardContainer = document.querySelector('.tech-container');
+const projectCardContainer = document.querySelector('.project-card-container');
+const buttonsProject = document.querySelectorAll('.category');
 
-export { navbar, burgerButton, exitButton, sidebar, overlay, blogContainer };
+export { navbar, burgerButton, exitButton, sidebar, overlay, blogContainer, selected, selectContainer, toolsCardContainer, techCardContainer, projectCardContainer, buttonsProject };
